0629d9b397f56003a4776d5233fdace2d42e6fb6
Hamish Moffatt <hamish@moffatt.id.au>
2020-07-30 19:28:56 +1000

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # pam-keycloak
 PAM module for authenticating against Keycloak
+
+# Installation
+1. Install libpam-python
+1. Fetch source
+1. Create virtualenv
+1. Install requirements
+1. Add to pam.d
+
+# Keycloak configuration
+Create new OpenID Connect client. Add client ID and secret to config file.
+
+## With OTP enabled
+First need new authentication flow to allow OTP to be bypassed
+Clone the direct flow grant as 'direct flow no OTP' and disable OTP
+
+Secondly create new client of type OpenID Connect with access type confidential
+In authentication flow overrides, set direct grant flow to the new direct grant flow
+created earlier
+
+# Test
+use pamtester
---
+++ pam_keycloak.py
@@ -0,0 +1,90 @@
+import os.path
+activate_this = os.path.join(os.path.dirname(os.path.realpath(__file__)), '.pyvenv/bin/activate_this.py')
+exec(compile(open(activate_this).read(), activate_this, 'exec'), dict(__file__=activate_this))
+
+import syslog
+from dotenv import load_dotenv
+from keycloak import KeycloakOpenID
+from keycloak.exceptions import KeycloakError
+
+
+DEFAULT_USER = "nobody"
+
+syslog.openlog(logoption=syslog.LOG_PID, facility=syslog.LOG_AUTH)
+
+
+def pam_syslog(prio, pamh, choice, message):
+    syslog.syslog(prio, "pam_keycloak(%s:%s): %s" % (pamh.service, choice, message))
+
+
+def pam_sm_authenticate(pamh, flags, argv):
+    try:
+        user = pamh.get_user(None)
+    except pamh.exception, e:
+        return e.pam_result
+    if user is None:
+        pamh.user = DEFAULT_USER
+
+    config_file = argv[1]  # argv[0] is the script name
+    if not os.path.isabs(config_file):
+        config_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), config_file)
+
+    load_dotenv(dotenv_path=config_file, verbose=True)
+
+    try:
+        # Configure client
+        keycloak_openid = KeycloakOpenID(server_url=os.getenv('SERVER_URL'),
+                                         realm_name=os.getenv('REALM_NAME'),
+                                         client_id=os.getenv('CLIENT_ID'),
+                                         client_secret_key=os.getenv('CLIENT_SECRET_KEY'))
+
+        # Get WellKnow
+        config_well_know = keycloak_openid.well_know()
+
+    except KeycloakError, e:
+        pam_syslog(syslog.LOG_NOTICE, pamh, "auth", "unable to authenticate for %s: %d %s" % (user, e.response_code, e.error_message))
+        return pamh.PAM_AUTHINFO_UNAVAIL
+
+    if pamh.authtok is None:
+        passmsg = pamh.Message(pamh.PAM_PROMPT_ECHO_OFF,
+                               "Password: ")
+        res = pamh.conversation(passmsg)
+        pamh.authtok = res.resp
+
+    try:
+        token = keycloak_openid.token(user, pamh.authtok)
+
+        # Potentially fetch the user info and check for specific claims here:
+        # userinfo = keycloak_openid.userinfo(token['access_token'])
+
+        return pamh.PAM_SUCCESS
+
+    except KeycloakError as e:
+        pam_syslog(syslog.LOG_NOTICE, pamh, "auth", "authentication failure for %s: %d %s" % (user, e.response_code, e.error_message))
+
+        if e.response_code == 401:
+            return pamh.PAM_AUTH_ERR
+
+        return pamh.PAM_AUTHINFO_UNAVAIL
+
+    return pamh.PAM_AUTH_ERR
+
+
+def pam_sm_setcred(pamh, flags, argv):
+    return pamh.PAM_SUCCESS
+
+
+def pam_sm_acct_mgmt(pamh, flags, argv):
+    return pamh.PAM_SUCCESS
+
+
+def pam_sm_open_session(pamh, flags, argv):
+    return pamh.PAM_SUCCESS
+
+
+def pam_sm_close_session(pamh, flags, argv):
+    return pamh.PAM_SUCCESS
+
+
+def pam_sm_chauthtok(pamh, flags, argv):
+    return pamh.PAM_SUCCESS
